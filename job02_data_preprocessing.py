7edb7d7fe77b70df7738c289ecad93648db4a2fa
chipmunk-tail <j1997m@naver.com>
2025-02-10 17:43:59 +0900

    update code

--- job02_data_preprocessing.py
+++ job02_data_preprocessing.py
@@ -8,6 +8,7 @@
 import json
 import pandas as pd
 from konlpy.tag import Okt
+import numpy as np
 import re
 
 # 데이터 위치
@@ -38,12 +39,6 @@ for txt in df.text:
     txt = re.sub(r'\n', ' ', txt)
     txt = re.sub(r'\r', ' ', txt)
 
-    start_idx = txt.find("== 개요 ==")               # 개요를 찾는다
-    if start_idx == -1:                             # 개요가 없다면
-        txt = ''                                    # text를 null값으로 대입한다.
-    end_idx = txt.find("==", (start_idx + 8))       # 개요 끝부분을 찾는다. 두번째 매개변수는 start 지점
-    txt = txt[start_idx : end_idx]
-
     txt = re.sub('[^가-힣]', ' ', txt)
     print(txt)
 
@@ -54,6 +49,7 @@ for txt in df.text:
     df_token = df_token[(df_token['class'] == 'Noun') |
                         (df_token['class'] == 'Verb') |
                         (df_token['class'] == 'Adjective')]
+    df_token['word'] = df_token['word'].replace('되어다', '되다')
     print(df_token)
 
     words = []
@@ -66,10 +62,16 @@ for txt in df.text:
     print(cleand_sentences)
 
 
+df.loc[:, 'text'] = cleand_sentences
+df.loc[:, 'text'] = df['text'].replace('', np.nan)  # 빈 문자열을 NaN으로 변경
+df.loc[:, 'text'] = df['text'].apply(lambda x: np.nan if isinstance(x, str) and x.strip() == '' else x)
+df.dropna(subset=['text'], inplace=True)
 
-df.text = cleand_sentences
-df.dropna(inplace = True)
-df.to_csv('./crawling_data/namuwiki_cleaned_data.csv', index = False)
+print(f"cleand_sentences 행의 수: {len(cleand_sentences)}")
+print(f"df_modify 행의 수: {len(df)}")
+print(df.head())
+
+df.to_csv('./data/namuwiki_cleaned_data.csv', index = False)
 
 
 
---
+++ job05_recommendation.py
@@ -0,0 +1,117 @@
+
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+from gensim.models import Word2Vec
+from scipy.io import mmread
+import pickle
+from PyQt5.QtCore import QStringListModel
+from PyQt5.QtCore import Qt
+
+
+from_window = uic.loadUiType('./namu_recommendation.ui')[0]
+
+
+class Exam(QWidget, from_window):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.Tfidf_matrix = mmread('./models/Tfidf_namuwiki.mtx').tocsr()
+        with open('./models/tfidf.pickle', 'rb') as f:
+            self.Tfidf = pickle.load(f)
+        print("Loading Word2Vec model...")
+        self.embedding_model = Word2Vec.load('./models/word2vec_namuwiki.model')
+        print("Word2Vec model loaded successfully!")
+
+
+        self.df_namu = pd.read_csv('./data/namuwiki_cleaned_data.csv')
+        self.titles = list(self.df_namu.title)
+        self.titles.sort()
+        # self.cb_title.addItem('Test01')
+        for title in self.titles:
+            self.cb_title.addItem(title)
+
+        # 자동완성 코드
+        model = QStringListModel()
+        model.setStringList(self.titles)
+        completer = QCompleter()
+        completer.setModel(model)
+        self.le_keyword.setCompleter(completer)
+
+
+        self.cb_title.currentIndexChanged.connect(self.combobox_slot)
+        self.btn_recommend.clicked.connect(self.btn_slot)
+
+
+    def btn_slot(self):
+        keyword = self.le_keyword.text()
+        if keyword in self.titles:
+            recommendation = self.recommendation_by_title(keyword)
+        else:
+            recommendation = self.recommendation_by_keyword(keyword)
+
+        # recommendation = self.recommendation_by_keyword(keyword)
+        if recommendation:
+            self.lbl_recommendation.setText(recommendation)
+
+
+    def combobox_slot(self):
+        title = self.cb_title.currentText()
+        print(title)
+        recommendation = self.recommendation_by_title(title)
+        self.lbl_recommendation.setText(recommendation)
+
+
+    def recommendation_by_title(self, tlt):
+        namu_idx = self.df_namu[self.df_namu.title == tlt].index[0]
+        cosine_sim = linear_kernel(self.Tfidf_matrix[namu_idx], self.Tfidf_matrix)
+        recommendation = self.getRecommendation(cosine_sim)
+        recommendation = '\n'.join(list(recommendation))
+        return recommendation
+
+    def recommendation_by_keyword(self, keyword):
+        try:
+            sim_word = self.embedding_model.wv.most_similar(keyword, topn = 10)
+        except:
+            self.lbl_recommendation.setText("제가 모르는 단어에요.")
+            return 0
+        words = [keyword]
+        for word, _ in sim_word:
+            words.append(word)
+        print(words)
+        sentence = []
+        count = 10
+        for word in words:
+            sentence = sentence + [word] * count
+            count -= 1
+        sentence = ' '.join(sentence)
+        sentence_vec  = self.Tfidf.transform([sentence])
+        cosine_sim = linear_kernel(sentence_vec, self.Tfidf_matrix)
+        recommendation = self.getRecommendation(cosine_sim)
+        recommendation = '\n'.join(list(recommendation))
+        return recommendation
+
+
+    def getRecommendation(self, cosine_sim):
+        simScore = list(enumerate(cosine_sim[-1]))
+        simScore = sorted(simScore, key=lambda x: x[1], reverse=True)
+        simScore = simScore[:11]
+        wikiIdx = [i[0] for i in simScore]
+        recmovieList = self.df_namu.iloc[wikiIdx, 0]
+        return recmovieList[1:11]
+
+
+if __name__ == '__main__':
+
+    # 노트북 윈도우 배열 문제
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)     # PyQt가 Windows 배율 설정을 감지하고 조절하도록 함
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)        # 아이콘, 그래픽 요소가 흐려지는 문제 해결
+
+    app = QApplication(sys.argv)
+    mainWindow = Exam()
+    mainWindow.show()
+    sys.exit(app.exec_())
