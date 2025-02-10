3c2c192f5317ed23dead80dc1dd4e4d906551375
chipmunk-tail <j1997m@naver.com>
2025-02-10 10:32:41 +0900

    update job01_resize_data.py

--- job01_resize_data.py
+++ job01_resize_data.py
@@ -1,17 +1,19 @@
 # 나무위키
 #
-#
+# 나무위키에서 제공한 가장 최근 덤프 파일을 이용해서 현제 읽고 있는 페이지 기반 연관 페이지 추천 프로그램
+# 사용한 Dump 파일은 20210301 버전 - 8.83GB
 #
 
+# 주의! 이 코드는 최소 메모리가 32GB임!
 
 import json
 
-# JSON 파일을 읽어오기
-with open('.data/raw_data/namuwiki_20210301.json', 'r', encoding='utf-8') as json_file:
-    data = json.load(json_file)
+# 변환할 json 파일을 읽어온다.
+with open('./data/raw_data/namuwiki_20210301.json', 'r', encoding='utf-8') as json_file:
+    raw_data = json.load(json_file)
 
-# 1/10로 나누어 저장 (전체 데이터의 10%)
-data_chunk = data[:len(data) // 10]
+# 전체 데이터의 10%저장 => 10의 몫으로 분리
+data_chunk = raw_data[:len(raw_data) // 10]
 
-with open('./data/namuwiki_resize.json', 'w', encoding='utf-8') as json_file:
+with open('./data/namuwiki_downsize.json', 'w', encoding='utf-8') as json_file:
     json.dump(data_chunk, json_file, ensure_ascii=False, indent=4)
