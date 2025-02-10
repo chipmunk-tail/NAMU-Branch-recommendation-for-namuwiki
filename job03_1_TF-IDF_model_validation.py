3e95f77b21dc3f25dff82f241a3d27c2964a84b2
chipmunk-tail <j1997m@naver.com>
2025-02-10 14:47:32 +0900

    add job03_1_TF-IDF_model_validation.py

---
+++ job03_1_TF-IDF_model_validation.py
@@ -0,0 +1,23 @@
+#
+#
+#
+
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.io import mmwrite, mmread
+import pickle
+
+
+df_namu = pd.read_csv('./data/namuwiki_cleaned_data_model_validation.csv')
+df_namu.info()
+
+Tfidf = TfidfVectorizer(sublinear_tf= True)
+Tfidf_matrix = Tfidf.fit_transform(df_namu.text)
+print(Tfidf_matrix.shape)
+
+with open('./models/tfidf.pickle', 'wb') as f:
+    pickle.dump(Tfidf, f)
+
+mmwrite('./models/Tfidf_namuwiki_model_validation.mtx', Tfidf_matrix)
