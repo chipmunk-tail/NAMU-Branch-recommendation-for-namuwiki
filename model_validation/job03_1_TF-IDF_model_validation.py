27d9aac618b346913a86d3ca28f7afe1326cff7e
chipmunk-tail <j1997m@naver.com>
2025-02-12 11:24:38 +0900

    parameter change

--- model_validation/job03_1_TF-IDF_model_validation.py
+++ model_validation/job03_1_TF-IDF_model_validation.py
@@ -22,10 +22,10 @@ df_namu = pd.read_csv(cleand_csv_path)
 df_namu.info()
 
 Tfidf = TfidfVectorizer(
-    sublinear_tf=True,
-    max_features=10000,                 # 가장 중요한 상위 10,000개 단어만 사용 (단어 수 줄이기)
-    min_df=5,                           # 5개 이하의 문서에 등장한 단어 제거 (희귀 단어 제거)
-    max_df=0.8                          # 80% 이상의 문서에서 등장한 단어 제거 (너무 자주 등장하는 단어 제거)
+    sublinear_tf = True,
+    max_features = 2000,                 # 가장 중요한 상위 10,000개 단어만 사용 (단어 수 줄이기)
+    min_df = 5,                           # 5개 이하의 문서에 등장한 단어 제거 (희귀 단어 제거)
+    max_df = 0.8                          # 80% 이상의 문서에서 등장한 단어 제거 (너무 자주 등장하는 단어 제거)
 )
 Tfidf_matrix = Tfidf.fit_transform(df_namu.text)
 print(Tfidf_matrix.shape)
